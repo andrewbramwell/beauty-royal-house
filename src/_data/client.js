a9ee890e6f5b50fcbe3a0443ecfcc83aeb01e422
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2024-12-21 12:20:25 +0000

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "Falkirk Thai Massage",
+    "email": "falkkirkthaimassage@gmail.com",
+    "phoneForTel": "+447564752559",
+    "phoneFormatted": "07564 752559",
+    "openingHours": "Mon-Sun: 10am-8pm",
+    "onlineBooking": "https://falkirkthaimassage.setmore.com/",
+    "address": {
+        "lineOne": "118 Main St",
+        "lineTwo": "Bainsford",
+        "city": "Falkirk",
+        "state": "",
+        "zip": "FK2 7PA",
+        "country": "UK",
+        "mapLink": "https://maps.app.goo.gl/hwRRnn2hBEmNaGwX7"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+        "googleReview": "",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://falkirkthaimassage.co.uk"
+};
